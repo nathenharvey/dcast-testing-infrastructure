dda600a9b1ea5b689ec5af88cb9950c7f34a6617
nathenharvey <nharvey@chef.io>
2015-03-20 07:16:18 -0400

    Add CentOS- and Ubuntu-specific unit tests

--- cookbooks/apache/spec/unit/recipes/default_spec.rb
+++ cookbooks/apache/spec/unit/recipes/default_spec.rb
@@ -27,4 +27,44 @@ describe 'apache::default' do
       expect(chef_run).to start_service "httpd"
     end
   end
+  
+  context 'on an CentOs platform' do
+
+    let(:chef_run) do
+      runner = ChefSpec::ServerRunner.new(:platform => "centos", :version => "6.4")
+      runner.converge(described_recipe)
+    end
+
+    it 'converges successfully' do
+      chef_run # This should not raise an error
+    end
+    
+    it 'installs the apache package' do
+      expect(chef_run).to install_package "httpd"
+    end
+    
+    it 'start the apache service' do
+      expect(chef_run).to start_service "httpd"
+    end
+  end
+  
+  context 'on an Ubuntu platform' do
+
+    let(:chef_run) do
+      runner = ChefSpec::ServerRunner.new(:platform => "ubuntu", :version => "12.04")
+      runner.converge(described_recipe)
+    end
+
+    it 'converges successfully' do
+      chef_run # This should not raise an error
+    end
+    
+    it 'installs the apache package' do
+      expect(chef_run).to install_package "apache2"
+    end
+    
+    it 'start the apache service' do
+      expect(chef_run).to start_service "apache2"
+    end
+  end
 end
